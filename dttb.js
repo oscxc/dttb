3f4dec6cae32733ad5cd10754cdfdd1f6f401969
cxc <15077550@qq.com>
2017-07-27 17:42:33 +0800

    dttb先提交，今天写不完了，明天继续

--- dttb.js
+++ dttb.js
@@ -24,11 +24,84 @@
             throw new TypeError("该对象不支持遍历");
         }
     }
+    function create(tagName,attrs) {
+        var el;
+        switch (tagName){
+            case "text":
+                el = document.createTextNode(attrs);
+                break;
+            case "comment":
+                el = document.createComment(attrs);
+                break;
+            case "fragment":
+                el = document.createDocumentFragment();
+                break;
+            default:
+                el = document.createElement(tagName);
+                if(attrs){
+                    each(attrs,function (k,v) {
+                        el.setAttribute(k,v);
+                    });
+                }
+                break;
+        }
+        return el;
+    }
+    function compare(p) {
+        function cmp(x,y) {
+            var tx = type(x), ty = type(y);
+            return tx==="String"?ty==="String"?x.localeCompare(y):1:ty==="String"?-1:x<y?-1:x>y?1:0;
+        }
+        return p?function (m,n) {
+            return cmp(m[p],n[p]);
+        }:cmp;
+    }
 
+    window.dttb = function(wrapper,prop){
+        var table = create("table",prop);
+        document.getElementById(wrapper).appendChild(table);
 
-    window.dttb = function(){
-        var data = null;
+        this.table = table;
+        this.create = create;
 
+        var thead = create("thead");
+        var tbody = create("tbody");
+        var tfoot = create("tfoot");
+        this.thead = thead;
+        this.tbody = tbody;
+        this.tfoot = tfoot;
+
+        this.add_thead = function () {
+            table.appendChild(thead);
+        };
+        this.add_tbody = function () {
+            table.appendChild(tbody);
+        };
+        this.add_tfoot = function () {
+            table.appendChild(tfoot);
+        };
+
+        this.add_trth = function (arr) {
+            var tr = create("tr");
+            each(arr,function (k,v) {
+                var th = create("th");
+                th.innerHTML = v;
+                tr.appendChild(th);
+            });
+            thead.appendChild(tr);
+        };
+
+        this.data = null;
+        this.add_trtd = function (arr,index) {
+            var rowData = arr[index];
+            var tr = create("tr");
+            each(rowData,function (k,v) {
+                var tr = create("tr");
+                th.innerHTML = v;
+                tr.appendChild(th);
+            });
+            tbody.appendChild(tr);
+        };
         this.orderBy = function (p) {
 
         };
